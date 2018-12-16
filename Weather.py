1f0aab91c0cf92a0ebed33544c0f764fe434133c
bonnappart <bonnegaut@gmail.com>
2018-12-16 18:19:48 +0100

    MAJ

--- Weather.py
+++ Weather.py
@@ -3,14 +3,8 @@ from random import random, gauss
 def weather():
     '''Update weather state in the shared memory'''
     temperature, type_of_weather = current_weather_in_shared_memory
-    return (fonction_meteo_selon_gauss(temperature), 4* random())
-
-def fonction_meteo_selon_gauss(degre):
-    variation = degre - 10
-    if variation > 0:
-        return degre + gauss(-1, variation)
-    else:
-        return degre + gauss(1, variation)
+    ensoleillement = 0
+    return (fonction_meteo_selon_data(t), 4* random(), ensoleillement)
 
 
 def fonction_meteo_selon_data(t):
