1b249a8168ec3218780fc0201fd3e4103f6b37f5
bonnappart <bonnegaut@gmail.com>
2018-12-17 17:11:52 +0100

    MAJ

--- Weather.py
+++ Weather.py
@@ -1,12 +1,14 @@
 from random import random, gauss
 
 def weather():
-    '''Update weather state in the shared memory'''
+    '''Update weather state in the shared memory
+    return temperature, weather state, '''
     temperature, type_of_weather = current_weather_in_shared_memory
-    ensoleillement = 0
-    return (fonction_meteo_selon_data(t), 4* random(), ensoleillement)
+    sunlight = fct_sunlight(t)  # return sunlight according to the day the clock is at
+    Array.Value = [Temp_function(t), 4*random(), sunlight]
 
 
-def fonction_meteo_selon_data(t):
-    pass
+def Temp_function(t, data):
+    '''return new temperature according to the day temperature of a weather station stored in data'''
+    return data[t]
 
