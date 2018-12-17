1a2e5527264e62e4fe2461745ad4f1749516c4c8
Yarkane <45920565+Yarkane@users.noreply.github.com>
2018-12-17 16:52:07 +0100

    Update House.py

--- House.py
+++ House.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def Maison(i):
-    NumeroMaison = i
-    Consommation, Comportement, Argent = Randomisaton()
-    while True:
-        if Clock1_Tick :
-            Weather = ObtenirValeurWeather()
-            EnergieProduite = Production(Weather)
-            Resultante = EnergieProduite - Consommation
-            EnvoiMarket(NumeroMaison,Resultante,Comportement)
+def House(i):
+    HouseIdentifier = i
+    Consommation, Comportement, Money, IncomePerMonth = Randomisation() #We will randomise, using statistics ressources, the data of the house.
+    while True: 
+        if Clock.Value == 1 : #The value of the shared memory has been updated by the Clock : it is the turn of the Houses to calculate their part
+            Money += IncomePerMonth/30 #The house win money with the work of the family
+            Weather = Weather.Value #The value of the shared memory has been updated by the Weather Process
+            CreatedEnergy = Production(Weather)
+            Rest = CreatedEnergy - Consommation
+            SendMarket(HouseIdentifier,Rest,Comportement) #We send to the market, via Message queues, the data of the house
+            while Clock.Value == 1 :
+                pass #Block until this is the turn of the other process to compute
         
         if Reception_Message_Market :
-            Argent += GainOuPerteParVenteOuAchat
-            
-        
+            Money += GainOrLoss #We update the income / outcome due to the energy market.
