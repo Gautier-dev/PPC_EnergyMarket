c78119641ec3f0cd4a942440d0ec0a0aa7e4c719
Yarkane <45920565+Yarkane@users.noreply.github.com>
2018-12-17 17:18:12 +0100

    Add files via upload

---
+++ external.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import time
+import random
+
+def External():
+    while true
+        time.wait(300)
+        factor = random.randint(1,100)
+        if factor > 98 :
+            if factor == 99 :
+                SendDiplomaticTensionSignal() #Communication through signals
+            else:
+                SendInondationSignal() #Communication through signals
