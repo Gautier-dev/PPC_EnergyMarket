86fd263efa52494bb57984372d4fe99b824622c9
Yarkane <45920565+Yarkane@users.noreply.github.com>
2019-01-07 17:54:28 +0100

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -3,10 +3,17 @@
 import time
 import multiprocessing
 
-def ClockTick():
-    while True:
-        time.wait(2000)
-        Clock.value = (Clock.value + 1) % 2
+class ClockTick(multiprocessing.process):
+    def __init__(self,clock):
+        super().__init__()
+        self.clock=clock #Pointer of the shared memory
+    def run(self):
+        Clock=self.clock
+        while True:
+            time.wait(2000)
+            Clock.value = (Clock.value + 1) % 2
         
 Clock = multiprocessing.Value('i',1) #Définition de la shared memory
 
+tick = ClockTick(Clock)
+tick.start()
