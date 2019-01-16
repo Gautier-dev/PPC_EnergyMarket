1c19f69bd5f2bf609112177087551fba4ba05559
Yarkane <45920565+Yarkane@users.noreply.github.com>
2019-01-16 11:57:55 +0100

    Update House.py

--- House.py
+++ House.py
@@ -33,21 +33,24 @@ class House(multiprocessing.Process):
         return self.productionFactor * (self.weather[1] * 0.65)  # 0.65 = Energy production per hour of sunlight
 
     def consommation(self):
-        if self.weather[0] == 0:
         return self.consommationFactor / self.weather[0]
 
     def run(self):
+        client = sysv_ipc.MessageQueue(-2)  # to send messages to the House Queue
         while True:
             if self.clock == 1:  # The value of the shared memory has been updated by the Clock : it is the turn of the
                 # Houses to calculate their part
-                self.Money += self.salary / 30  # The house win money with the work of the family
+                self.Money += self.salary / 30  # The house win money with the work of the family 
                 created_energy = self.Production()
                 self.SurplusOrNeed = created_energy - self.consommation()
-                client = sysv_ipc.MessageQueue(-2)  # to send messages to the House Queue
+                
                 if self.SurplusOrNeed > 0 and self.Behavior != 3:
                     message = str((self.i, self.SurplusOrNeed)).encode()
                     client.send(message)  # Send its rest to the House Queue if giver
-                    while self.clock == 1:  # wait until the end of the day
+                    
+                    #TODO : Update Surplus (sinon il va gagner de l'argent sur de l'énergie qu'il donne)                    
+                    
+                    while self.clock.value == 1:  # wait until the end of the day
                         pass
                 else:
                     # receive energy from other houses
@@ -57,27 +60,30 @@ class House(multiprocessing.Process):
                             i, value = literal_eval(msg.decode())
                             if value > self.SurplusOrNeed:
                                 message = str((i, value - self.SurplusOrNeed)).encode()
+                                self.SurplusOrNeed = 0
                                 self.lock.acquire()
                                 client.send(message)
                                 self.lock.release()
                             else:
-                                if self.Behavior != 3:
-                                    message = str((self.i, self.SurplusOrNeed - value)).encode()
-                                    self.lock.acquire()
-                                    client.send(message)
-                                    self.lock.release()
+                                
+                                #TODO : gautier
+                                
+                                message = str((self.i, self.SurplusOrNeed - value)).encode()
+                                self.lock.acquire()
+                                client.send(message)
+                                self.lock.release()
 
             if self.clock.value == 0:  # it's where the houses send surplus or need to the market
+                
                 client_market = sysv_ipc.MessageQueue(-1)
                 if self.Behavior == 2 or self.Behavior == 3:
                     if self.SurplusOrNeed != 0:
                         client_market.send(self.SurplusOrNeed)
-                message, t = client_market.receive()
-                value = literal_eval(message.decode())
-                self.Money = self.Money + value
-
-
-
-
-
-
+                
+                while self.clock.value == 0:              
+                    if client_market.current_messages > 0 :                    
+                        message, t = client_market.receive()
+                        value = message.decode()
+                        self.Money = self.Money + value
+                        while self.clock.value == 0: 
+                            pass
