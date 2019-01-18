c52239d15dd143465c5ea8b82c2f3aeaaeaf6c79
bonnappart <bonnegaut@gmail.com>
2019-01-18 22:06:52 +0100

    Comment and format

--- Market.py
+++ Market.py
@@ -25,9 +25,10 @@ class Market(multiprocessing.Process):
     When the Clock.Value is equal to 0, it calculates the price of the energy and tell the houses what they have to pay to receive it.
     """
     
-    def __init__(self,externalFactors,lockExternal,globalNeed,lockGlobalNeed,payableEnergyBank,lockPayable,clocker,weather,child_conn):
+    def __init__(self, externalFactors, lockExternal, globalNeed, lockGlobalNeed, payableEnergyBank, lockPayable,
+                 clocker, weather, child_conn):
         super().__init__()
-        self.mq = sysv_ipc.MessageQueue(-1, sysv_ipc.IPC_CREAT)
+        self.mq = sysv_ipc.MessageQueue(-1, sysv_ipc.IPC_CREAT)  #init message queue of the market
         self.firstTime = 1 #Boolean : 1 = first time used, we don't have to calculate the price of the energy
         
         #Shared memory pointers
@@ -49,7 +50,7 @@ class Market(multiprocessing.Process):
         """
         This function calculates the price of the energy.
         The more energy the people need and he less energy the bank has, the more expensive it would be.
-        We also have to check the externalfactors and the weather
+        We also have to check the externalFactors and the weather
         """
         
         # External factors : a counter of disasters which increases the price
@@ -93,15 +94,16 @@ class Market(multiprocessing.Process):
     def run(self):
         external = External.externalProcess()
         signal.signal(signal.SIGINT, self.handler) #associate the SIGINT signal to the handler
-        external.start()
         print("start external")
+        external.start()
+
         
         price = 100 #Price at the beginning of the simulation
             
     
         while True:
-            #print("message restant MQ Market : ", self.mq.current_messages)
-            while self.mq.current_messages > 0 and self.clock.value == 0 : #While there is data sent by the houses.
+            #  print("message restant MQ Market : ", self.mq.current_messages)
+            while self.mq.current_messages > 0 and self.clock.value == 0: #While there is data sent by the houses.
                 message, t = self.mq.receive()
                 value = ast.literal_eval(message.decode())
                 #Value : (HouseIdentifier,RestOrNeed)
@@ -110,25 +112,31 @@ class Market(multiprocessing.Process):
                 thread = threading.Thread(target=self.interprete, args=(restOrNeed, price, houseIdentifier))
                 thread.start()
                     
-            #print("Clock recupere dans market : ", self.clock.value)
+            #  print("Clock recupere dans market : ", self.clock.value)
             
-            if self.clock.value == 1: #The value of the shared memory has been updated by the Clock : it is the turn of the Market to calculate its part
+            if self.clock.value == 1:  #  The value of the shared memory has been updated by the Clock : it is the turn
+                # of the Market to calculate its part
                 if self.firstTime == 1:
                     self.firstTime = 0
                 else:
-                    #We have to acquire all the locks :
+                    #  We have to acquire all the locks :
                     self.lockGlobalNeed.acquire()
                     self.lockPayable.acquire()
                     self.lockExternal.acquire()
+
+                    #  These values are just information about a given day
                     payableEnergyWanted = self.globalNeed.value
                     totalPrice = price * payableEnergyWanted
+
                     price = self.priceCalculation(payableEnergyWanted, self.energyBank.value, self.externalFactors.value, price) #Using a linear model
                     
-                    #Print information
+                    #  Print information
                     result = [str(price), str(self.externalFactors.value), str(totalPrice)]
-                    print("The price of the energy is : {}.\nThe number of disasters which occured today is : {}.\nThe price of the energy for the whole community is : {}.\n".format(result[0],result[1],result[2]))
-                    #self.pipe.send(display)
-                    
+                    print("The price of the energy is : {}.\nThe number of disasters which occured today is : {}.\nThe "
+                          "price of the energy for the whole community is : {}.\n".format(result[0], result[1], result[2]))
+                    #  self.pipe.send(display)
+
+                    #  Reset of these values everyday
                     self.energyBank.value, self.globalNeed.value, self.externalFactors.value = 0, 0, 0
                     
                     self.lockGlobalNeed.release()
